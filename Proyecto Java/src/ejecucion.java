93841a6cca583e613e8343c50b010fd6eab8698f
dpeite <pepeluisjose06@gmail.com>
2013-11-23 18:15:10 +0100

    Arreglado departamento

--- Proyecto Java/src/ejecucion.java
+++ Proyecto Java/src/ejecucion.java
@@ -8,6 +8,7 @@ public class ejecucion {
 	public static void insertapersona (String[] str){
 		String str1[];
 		String str2[];
+		String str3[];
 		str1=separar.espacios(str[0]);
 		str2=separar.espacios(str[2]);
 
@@ -15,7 +16,9 @@ public class ejecucion {
 			leer.getpersonas().put(getid(),new alumno(str[1], getid(),str2[1],str1[1],str2[2],Integer.parseInt(str2[3].trim()),"", ""));
 		}
 		if(str1[1].equals("profesor")){
-			leer.getpersonas().put(getid(), new profesor(str[1], getid(),str2[1],str[1],str2[2],Integer.parseInt(str2[3].trim()),""));
+			str3=separar.espacios(str[4]);
+			System.out.println(str3[1]);
+			leer.getpersonas().put(getid(), new profesor(str[1], getid(),str2[0],str[1],str[3],Integer.parseInt(str3[1].trim()),""));
 		}
 	}
 	private static Integer getid(){
